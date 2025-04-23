a0a856b97dddf9164784805a27c73c3a38bb4450
iamanggi <anggipuspita452@gmail.com>
2025-04-23 20:44:10 +0700

    Hitung total harga berdasarkan jumlah makanan dan minuman

--- lib/order_page.dart
+++ lib/order_page.dart
@@ -18,5 +18,12 @@ class _OrderPageState extends State <OrderPage> {
     // Ambil nilai jumlah makanan dan minuman dari input, ubah ke int, jika gagal parsing jadikan 0
     int jumlahMakanan = int.tryParse(jumlahMakananController.text) ?? 0;
     int jumlahMinuman = int.tryParse(jumlahMinumanController.text) ?? 0;
+
+    setState(() {
+  // Menghitung total harga berdasarkan jumlah makanan dan minuman
+  // dan memperbarui tampilan UI dengan nilai total harga yang baru
+  totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman * 5000);
+});
+
   }
 }
