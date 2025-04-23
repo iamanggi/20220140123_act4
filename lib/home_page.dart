cdd5fa10888d3768a9521e8375480b6c2c16bec3
iamanggi <anggipuspita452@gmail.com>
2025-04-23 20:28:21 +0700

    Buat halaman Home dengan tombol navigasi ke halaman Order

---
+++ lib/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+ State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page')),
+        body: Center(
+          child: Column(
+            spacing: 8,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Selamat Datang di Home Page!'),
+              ElevatedButton(
+                onPressed: () {  
+                Navigator.push(
+                  context, 
+                MaterialPageRoute(builder: (context)=> const OrderPage()),
+                );
+                }, 
+                child: Text('Order Now')),
+            ],
+          ),
+        )
+    );
+  }
+}
