95023ffa493dfc75526f4ea4b2ac29c64a87b104
iamanggi <anggipuspita452@gmail.com>
2025-04-23 20:41:54 +0700

    Tambah fungsi hitung total pesanan

--- lib/order_page.dart
+++ lib/order_page.dart
@@ -13,4 +13,10 @@ class _OrderPageState extends State <OrderPage> {
   final TextEditingController jumlahMakananController = TextEditingController();
   final TextEditingController jumlahMinumanController = TextEditingController();
   int totalHarga = 0;
+
+  void calculateTotalPrice(){
+    // Ambil nilai jumlah makanan dan minuman dari input, ubah ke int, jika gagal parsing jadikan 0
+    int jumlahMakanan = int.tryParse(jumlahMakananController.text) ?? 0;
+    int jumlahMinuman = int.tryParse(jumlahMinumanController.text) ?? 0;
+  }
 }
