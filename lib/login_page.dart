426eb63c73e9b05d44a41f530cb88be0fe3879b6
iamanggi <anggipuspita452@gmail.com>
2025-04-23 19:52:44 +0700

    buat tampilan LoginPage dengan input email dan validasi

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,6 +12,38 @@ class _LoginPageState extends State <LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey <FormState>();
+    return Scaffold(
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email', hintText: 'Enter your email'
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) { 
+                    return 'Please enter your email';
+                    }
+                    return null;
+                },
+              )
+            ],
+          ),)
+      ),
+    );
+  }
 }
 
 
